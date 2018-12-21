767a40d2f620d02556d1397ed206196b2d0277e2
spam88 <46036714+spam88@users.noreply.github.com>
2018-12-21 02:35:13 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Hassan_Gamer");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("515310203357495326")
+setInterval(function() {
+channel.send(`========`)
+}, 25)
+})
+ 
+ 
+client.login("NTE0OTAxMTg0NjIwOTIwODU0.DtdTjw.6C-A1FcO1FM9dQdiAKBOztGT_Nw");
